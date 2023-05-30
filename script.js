de984bf2f0622073f72c9728a8f471be710a5fe2
Mohammad <mohdhashirmazaher@gmail.com>
2023-05-29 23:53:39 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById('addBtn').addEventListener('click', function() {
+    performOperation('+');
+  });
+  
+  document.getElementById('subtractBtn').addEventListener('click', function() {
+    performOperation('-');
+  });
+  
+  document.getElementById('multiplyBtn').addEventListener('click', function() {
+    performOperation('*');
+  });
+  
+  document.getElementById('divideBtn').addEventListener('click', function() {
+    performOperation('/');
+  });
+  
+  function performOperation(operator) {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+  
+    if (!isNaN(num1) && !isNaN(num2)) {
+      var result;
+      if (operator === '+') {
+        result = num1 + num2;
+      } else if (operator === '-') {
+        result = num1 - num2;
+      } else if (operator === '*') {
+        result = num1 * num2;
+      } else if (operator === '/') {
+        if (num2 !== 0) {
+          result = num1 / num2;
+        } else {
+          document.getElementById('result').textContent = 'Error: Cannot divide by zero.';
+          return;
+        }
+      }
+  
+      document.getElementById('result').textContent = 'Result: ' + result;
+    } else {
+      document.getElementById('result').textContent = 'Please enter valid numbers.';
+    }
+  }
+  
